3736d4b5d054ad9c805b05608b8e872cedb91cbf
Poonam <poonamchaursia1998@gmail.com>
2021-07-14 17:55:31 +0530

    first React commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,92 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import  "./index.css";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+<>
+<div className="heading">
+    <div className="heading-content">
+        <div className="Brand-Logo">
+            <h1>Web Designing</h1>
+        </div>
+        <div className="main-info">
+            <ul className="services-details">
+                <li><a href="#">Service</a></li>
+                <li><a href="#">Products</a></li>
+                <li><a href="#">About US</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Blogs</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+<section className="section_about">
+    <div className="subHeader-title">
+        <span>We are Awesome Web Developers</span>
+    </div>
+    <div className="know_more">
+        <a href="#">Get Started</a>
+        <a href="#">Know More</a>
+    </div>
+</section>
+
+<section className="main_div">
+    <div class="about_div">
+                    <h3>K:Watch</h3>
+                    <h5>Product Details</h5>
+                    <p>It was a story of the the Diligent Girl whose father married the Lazy Girl's Mother. 
+                   
+                    </p>
+                   
+            </div>
+            <div class="about_div">
+                    <h3>K:LookUp</h3>
+                    <h5>Product Details</h5>
+                    <p>It was a story of the the Diligent Girl whose father married the Lazy Girl's Mother. 
+                   
+                    </p>
+                   
+             </div>
+            <div class="about_div">
+                    <h3>K:Litigation BI</h3>
+                    <h5>Product Details</h5>
+                    <p>It was a story of the the Diligent Girl whose father married the Lazy Girl's Mother. 
+                    
+                    </p>
+                   
+            </div>
+            <div class="about_div">
+                    <h3>Dorimon</h3>
+                    <h5>Product Details</h5>
+                    <p>It was a story of the the Diligent Girl whose father married the Lazy Girl's Mother. 
+                    The Stepmother sent the Diligent Girl away to find a job and make some money. ... Then she came upon a fairy's house who offered her a job to clean the house for one year. The fairies said, “There are seven rooms in this house.
+                    </p>
+                   
+            </div>
+    {/* <div className="img-div">
+        <figure>
+            <img src="images/img5.jpg" alt="missingImg"/>
+        </figure>
+    </div> */}
+</section>
+
+
+
+
+
+{/* <div className="content">
+    <h1>Hello React</h1>
+    <p>List of 5 Netflix Series</p>
+
+    <ol className="list_Series">
+        <li>DARK</li>
+        <li>AWESOME</li>
+    </ol>
+</div> */}
+
+
+</>, document.getElementById('root')
+
+);
